6611910d9b48a7bc6e4837430c508c2fc0c7fb1e
WooLamLee <98564214+w00lam@users.noreply.github.com>
2026-04-24 14:43:32 +0900

    Initial commit

---
+++ src/main/java/com/ildang100/backoffice/Ildang100BackofficeApplication.java
@@ -0,0 +1,13 @@
+package com.ildang100.backoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ildang100BackofficeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ildang100BackofficeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ildang100/backoffice/Ildang100BackofficeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ildang100.backoffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ildang100BackofficeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
