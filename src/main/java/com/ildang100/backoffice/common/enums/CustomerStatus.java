73870a215554429330e966aa7e3f9cdd3f6cc45b
WooLamLee <98564214+w00lam@users.noreply.github.com>
2026-04-24 23:38:28 +0900

    chore: Enum 주석 추가

--- src/main/java/com/ildang100/backoffice/common/enums/CustomerStatus.java
+++ src/main/java/com/ildang100/backoffice/common/enums/CustomerStatus.java
@@ -1,5 +1,16 @@
 package com.ildang100.backoffice.common.enums;
 
+/**
+ * 고객 상태를 정의하는 Enum입니다.
+ *
+ * <p>고객 계정의 활성 여부를 나타냅니다.</p>
+ *
+ * <ul>
+ *     <li>ACTIVE - 정상적으로 서비스 이용 가능</li>
+ *     <li>INACTIVE - 비활성 상태</li>
+ *     <li>SUSPENDED - 이용 제한 상태</li>
+ * </ul>
+ */
 public enum CustomerStatus {
     ACTIVE,
     INACTIVE,
--- src/main/java/com/ildang100/backoffice/common/enums/OrderStatus.java
+++ src/main/java/com/ildang100/backoffice/common/enums/OrderStatus.java
@@ -1,5 +1,17 @@
 package com.ildang100.backoffice.common.enums;
 
+/**
+ * 주문 상태를 정의하는 Enum입니다.
+ *
+ * <p>주문의 처리 진행 상태를 나타냅니다.</p>
+ *
+ * <ul>
+ *     <li>PREPARED - 주문 준비 완료 상태</li>
+ *     <li>SHIPPING - 배송 중 상태</li>
+ *     <li>DELIVERED - 배송 완료 상태</li>
+ *     <li>CANCELED - 주문 취소 상태</li>
+ * </ul>
+ */
 public enum OrderStatus {
     PREPARED,
     SHIPPING,
