377fdd5120b7809c5a63713d629f1af11432051f
WooLamLee <98564214+w00lam@users.noreply.github.com>
2026-04-25 14:35:59 +0900

    docs: OrderStatus 주석 수정

--- src/main/java/com/ildang100/backoffice/common/enums/OrderStatus.java
+++ src/main/java/com/ildang100/backoffice/common/enums/OrderStatus.java
@@ -6,10 +6,10 @@ package com.ildang100.backoffice.common.enums;
  * <p>주문의 처리 진행 상태를 나타냅니다.</p>
  *
  * <ul>
- *     <li>PREPARED - 주문 준비 완료 상태</li>
+ *     <li>PREPARING - 주문 준비 중 상태</li>
  *     <li>SHIPPING - 배송 중 상태</li>
  *     <li>DELIVERED - 배송 완료 상태</li>
- *     <li>CANCELED - 주문 취소 상태</li>
+ *     <li>CANCELLED - 주문 취소 상태</li>
  * </ul>
  */
 public enum OrderStatus {
