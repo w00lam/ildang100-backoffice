95e840f7caa2fd0f4712843f14a324a7ac66d232
WooLamLee <98564214+w00lam@users.noreply.github.com>
2026-04-27 14:13:58 +0900

    fix: 회원가입 API HTTP 상태코드 201(CREATED) 정상 반환하도록 수정

--- src/main/java/com/ildang100/backoffice/auth/controller/AdminAuthController.java
+++ src/main/java/com/ildang100/backoffice/auth/controller/AdminAuthController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -33,6 +34,7 @@ public class AdminAuthController {
     private final AdminService adminService;
 
     @PostMapping("/admins/signup")
+    @ResponseStatus(HttpStatus.CREATED)
     public CommonApiResponse<Void> signup(@RequestBody @Valid AdminSignUpRequest request) {
         adminService.signUp(request);
 
