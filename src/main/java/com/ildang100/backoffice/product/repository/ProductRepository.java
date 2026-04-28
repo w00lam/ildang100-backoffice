c5a0efaeb6a198155f446a0274b08d6ae084107c
WooLamLee <98564214+w00lam@users.noreply.github.com>
2026-04-28 10:38:55 +0900

    feat: 재고 부족 상품 조회를 위한 Repository 메서드 추가

--- src/main/java/com/ildang100/backoffice/product/repository/ProductRepository.java
+++ src/main/java/com/ildang100/backoffice/product/repository/ProductRepository.java
@@ -4,4 +4,19 @@ import com.ildang100.backoffice.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    /**
+     * 재고가 특정 수량 이하인 상품 개수를 조회합니다.
+     *
+     * <p>
+     * 대시보드에서 재고 부족 상품 수를 계산하기 위해 사용됩니다.
+     * </p>
+     *
+     * @param quantity 기준 재고 수량
+     * @return 재고 부족 상품 수
+     *
+     * @author 이우람
+     * @since 2026-04-28
+     */
+    long countByStockLessThanEqual(int quantity);
 }
